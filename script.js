b12286758d0a24c3147ae7e6449127b80e07578b
ranamardikhi <160498566+518997@users.noreply.github.com>
2024-03-11 09:53:14 +0100

    518997

--- script.js
+++ script.js
@@ -1,32 +1,86 @@
-function search() {
+document.addEventListener('DOMContentLoaded', function() {
 
-var zoekQuery = document.getElementById('searchInput').value.trim();
+const searchBox = document.getElementById('searchBox');
 
-if (zoekQuery !== '') {
+const searchInput = document.getElementById('searchInput');
 
-window.location.href = '/reserveren.html?q=' + encodeURIComponent(zoekQuery);
+const searchButton = document.getElementById('searchButton');
+
+let isExpanded = false;
+
+searchButton.addEventListener('click', function() {
+
+if (!isExpanded) {
+
+expandSearchBox();
 
 } else {
 
-alert('Voer een zoekterm in!');
+collapseSearchBox();
 
 }
 
+});
+
+searchBox.addEventListener('submit', function(event) {
+
+event.preventDefault();
+
+search();
+
+});
+
+searchInput.addEventListener('keypress', function(event) {
+
+if (event.key === 'Enter') {
+
+event.preventDefault();
+
+search();
+
+}
+
+});
+
+function expandSearchBox() {
+
+searchBox.style.width = '200px';
+
+searchInput.style.width = '160px';
+
+isExpanded = true;
+
+}
+
+function collapseSearchBox() {
+
+searchBox.style.width = '50px';
+
+searchInput.style.width = '0';
+
+isExpanded = false;
+
+}
+
+function search() {
+
+var zoekQuery = searchInput.value.trim().toLowerCase();
+
 if (zoekQuery !== '') {
 
-if (zoekQuery === 'reserveren') {
+if (zoekQuery === 'home') {
+
+window.location.href = '/Index.html?q=' + encodeURIComponent(zoekQuery);
+
+} else if (zoekQuery === 'reserveren') {
 
 window.location.href = '/reserveren.html?q=' + encodeURIComponent(zoekQuery);
 
-} else if (zoekQuery === 'betaling') {
-
-window.location.href = '/betaling.html?q=' + encodeURIComponent(zoekQuery);
-
 } else if (zoekQuery === 'la-sarthe-travel') {
 
- window.location.href = '/la-sarthe-travel.html?q=' + encodeURIComponent(zoekQuery);
+window.location.href = '/la-sarthe-travel.html?q=' + encodeURIComponent(zoekQuery);
 
- } else if (zoekQuery === 'Michelin') {
+} else if (zoekQuery === 'michelin') {
 
 window.location.href = '/Michelin.html?q=' + encodeURIComponent(zoekQuery);
 
@@ -43,17 +97,5 @@ alert('Voer een zoekterm in!');
 }
 
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-
-document.getElementById('searchInput').value = '';
-
-document.getElementById('searchForm').addEventListener('submit', function(event) {
-
-search();
-
-event.preventDefault();
-
-});
 
 });
